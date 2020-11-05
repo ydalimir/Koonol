88da797d433bf85fec1ac50744866dcbedfee2f7
ydalimir <ydalimir12arevalo16@outlook.es>
2020-11-05 15:56:19 -0600

    Update Restaurants.js

--- app/screens/Restaurants/Restaurants.js
+++ app/screens/Restaurants/Restaurants.js
@@ -1,13 +1,38 @@
 
 import React from 'react';
-import {  Text, View } from 'react-native';
+import {  Text, View ,StyleSheet } from 'react-native';
+import {Icon} from 'react-native-elements'
 
 export default function Restaurants() {
   return (
-    <View >
+    <View style={styles.viewBody} >
       <Text> Restaurants</Text>
+      <Icon
+      reverse
+      type="material-comunity"
+      name="plus"
+      color="#e3e3e3"
+      containerStyle={styles.btnContainer}
+      
+      />
+
+     
       
     </View>
   );
 }
 
+
+const styles = StyleSheet.create({
+  viewBody: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  btnContainer: {
+    position: "absolute",
+    bottom: 10,
+    right:10
+
+  },
+  
+});
