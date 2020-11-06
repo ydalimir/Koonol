63174c0f89dd55eb28eff6235403430fa1e21ee0
ydalimir <ydalimir12arevalo16@outlook.es>
2020-11-06 03:27:17 -0600

    Ydalimir

--- app/components/Restaurants/AddRestaurantForm.js
+++ app/components/Restaurants/AddRestaurantForm.js
@@ -1,13 +1,237 @@
-import React from "react";
-import { View ,Text ,StyleSheet} from "react-native";
+
+import React, { useState, useEffect } from "react";
+import { StyleSheet, View, ScrollView, Alert, Dimensions,Text } from "react-native";
+import { Icon, Avatar, Image, Input, Button } from "react-native-elements";
+import * as Permissions from "expo-permissions";
+import * as ImagePicker from "expo-image-picker";
+import { map, size, filter } from "lodash";
 
 
-export default function AddRestaurant() {
-  
+export default function AddRestaurantForm(props) {
+
+    const { toastRef, setIsLoading, navigation } = props;
+    const [restaurantName, setRestaurantName] = useState("");
+    const [restaurantAddress, setRestaurantAddress] = useState("");
+    const [restaurantDescription, setRestaurantDescription] = useState("");
+    const [imagesSelected, setImagesSelected] = useState([]);
+
+    
+
+    const addRestaurant = () => {
+       
+        console.log(imagesSelected)
+      };
+    
+ 
 
   return (
-    <View>
-     <Text>crearemos tu restaurante</Text>
-    </View>
+    <ScrollView style={styles.scrollView}>
+       <FormAdd
+
+       setRestaurantName={setRestaurantName}
+       setRestaurantAddress={setRestaurantAddress}
+       setRestaurantDescription={setRestaurantDescription}
+
+      />
+      <UploadImage
+        toastRef={toastRef}
+        setImagesSelected={setImagesSelected}
+        imagesSelected={imagesSelected}
+        
+      />
+      <Button
+        title="Crear Restaurante"
+        onPress={addRestaurant}
+        buttonStyle={styles.btnAddRestaurant}
+      />
+
+    </ScrollView>
   );
 }
+
+function FormAdd(props) {
+
+
+    const {
+        setRestaurantName,
+        setRestaurantAddress,
+        setRestaurantDescription,
+        
+      } = props;
+   
+  
+    return (
+      <View style={styles.viewForm}>
+        <Input
+          placeholder="Nombre del restaurante"
+          containerStyle={styles.input}
+          onChange={(e) => setRestaurantName(e.nativeEvent.text)}
+          
+        />
+        <Input
+          placeholder="Dirección"
+          containerStyle={styles.input}
+          onChange={(e) => setRestaurantAddress(e.nativeEvent.text)}
+          
+        />
+        <Input
+          placeholder="Descripcion del restaurante"
+          multiline={true}
+          inputContainerStyle={styles.textArea}
+          onChange={(e) => setRestaurantDescription(e.nativeEvent.text)}
+          
+        />
+      </View>
+    );
+  }
+
+  function UploadImage(props) {
+    const { toastRef , setImagesSelected ,imagesSelected} = props;
+  
+    const imageSelect = async () => {
+      const resultPermissions = await Permissions.askAsync(
+        Permissions.CAMERA_ROLL
+      );
+  
+      if (resultPermissions === "denied") {
+        toastRef.current.show(
+          "Es necesario aceptar los permisos de la galeria, si los has rechazado tienes que ir ha ajustes y activarlos manualmente.",
+          3000
+        );
+      } else {
+        const result = await ImagePicker.launchImageLibraryAsync({
+          allowsEditing: true,
+          aspect: [4, 3],
+        });
+       
+        if (result.cancelled) {
+          toastRef.current.show(
+            "Has cerrado la galeria sin seleccionar ninguna imagen",
+            2000
+          );
+        } else {
+            setImagesSelected([...imagesSelected, result.uri]);
+        }
+      }
+    };
+
+    const removeImage = (image) => {
+        Alert.alert(
+          "Eliminar Imagen",
+          "¿Estas seguro de que quieres eliminar la imagen?",
+          [
+            {
+              text: "Cancel",
+              style: "cancel",
+            },
+            {
+              text: "Eliminar",
+              onPress: () => {
+                setImagesSelected(
+                  filter(imagesSelected, (imageUrl) => imageUrl !== image)
+                );
+              },
+            },
+          ],
+          { cancelable: false }
+        );
+      };
+    
+  
+  
+  
+    return (
+        <View style={styles.viewImages}>
+        {size(imagesSelected) < 4 && (
+          <Icon
+            type="material-community"
+            name="camera"
+            color="#7a7a7a"
+            containerStyle={styles.containerIcon}
+            onPress={imageSelect}
+          />
+        )}
+        {map(imagesSelected, (imageRestaurant, index) => (
+          <Avatar
+            key={index}
+            style={styles.miniatureStyle}
+            source={{ uri: imageRestaurant }}
+
+            onPress={() => removeImage(imageRestaurant)}
+            
+          />
+        ))}
+      </View>
+      
+    );
+  }
+  
+
+
+const styles = StyleSheet.create({
+  scrollView: {
+    height: "100%",
+  },
+  viewForm: {
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  input: {
+    marginBottom: 10,
+  },
+  textArea: {
+    height: 100,
+    width: "100%",
+    padding: 0,
+    margin: 0,
+  },
+  btnAddRestaurant: {
+    backgroundColor: "#00a680",
+    margin: 20,
+  },
+  viewImages: {
+    flexDirection: "row",
+    marginLeft: 20,
+    marginRight: 20,
+    marginTop: 30,
+  },
+  containerIcon: {
+    alignItems: "center",
+    justifyContent: "center",
+    marginRight: 10,
+    height: 70,
+    width: 70,
+    backgroundColor: "#e3e3e3",
+  },
+  miniatureStyle: {
+    width: 70,
+    height: 70,
+    marginRight: 10,
+  },
+  viewPhoto: {
+    alignItems: "center",
+    height: 200,
+    marginBottom: 20,
+  },
+  mapStyle: {
+    width: "100%",
+    height: 550,
+  },
+  viewMapBtn: {
+    flexDirection: "row",
+    justifyContent: "center",
+    marginTop: 10,
+  },
+  viewMapBtnContainerCancel: {
+    paddingLeft: 5,
+  },
+  viewMapBtnCancel: {
+    backgroundColor: "#a60d0d",
+  },
+  viewMapBtnContainerSave: {
+    paddingRight: 5,
+  },
+  viewMapBtnSave: {
+    backgroundColor: "#00a680",
+  },
+});
